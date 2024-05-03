aaa389b27cb25b934eb44f4abde351cffe2d43c3
TUMO Labs <tumolabs@TUMOs-iMac.local>
2024-05-03 04:49:51 +0400

    Improved parsing

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
-// example:
-// let userString = "I am learning JS with Pavel.";
 
 const userString = document.querySelector("#userString");
-
 const words = document.querySelector("#words");
 const letters = document.querySelector("#letters");
 const sentences = document.querySelector("#sentences");
 
-function countWords() {
-	let amountOfWords = userString.value.split(" ").length;
-	words.innerText = amountOfWords;
+function countLetters() {
+    let amountOfLetters = userString.value.replace(/[^a-zA-Z]/g, '').length;
+    letters.innerText = amountOfLetters;
 }
 
-function countLetters() {
-	let amountOfLetters = userString.value.split("").length;
-	letters.innerText = amountOfLetters;
+function countWords() {
+    let text = userString.value.trim(); 
+    let amountOfWords = text.match(/\b[\w']+\b/g).length;
+    words.innerText = amountOfWords;
 }
 
 function countSentences() {
-	let amountOfSentences = userString.value.split(".").length;
-	sentences.innerText = amountOfSentences;
+    let text = userString.value.trim();
+    let amountOfSentences = text.match(/[.!?]+/g).length; 
+    sentences.innerText = amountOfSentences;
 }
 
 function counter() {
-	countWords();
-	countLetters();
-	countSentences();
+    countWords();
+    countLetters();
+    countSentences();
+}
+
+function getRandomColor() { 
+    let red = Math.floor(Math.random() * 256); 
+    let green = Math.floor(Math.random() * 256); 
+    let blue = Math.floor(Math.random() * 256); 
+
+    let redHex = red.toString(16).padStart(2, '0'); 
+    let greenHex = green.toString(16).padStart(2, '0'); 
+    let blueHex = blue.toString(16).padStart(2, '0'); 
+
+    let colorCode = `#${redHex}${greenHex}${blueHex}`;
+
+    return colorCode;
 }
 
 function changeBackground() {
-	var colors = [
-		"#FF5733",
-		"#F7DC6F",
-		"#9B59B6",
-		"#85C1E9",
-		"#48C9B0",
-		"#F1948A",
-		"#D35400",
-	];
-	var randomColor = colors[Math.floor(Math.random() * colors.length)];
-	document.body.style.backgroundColor = randomColor;
+    let randomColor = getRandomColor();
+
+    document.body.style.backgroundColor = randomColor;
 }
+
+
